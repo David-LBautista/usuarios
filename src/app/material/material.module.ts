df90af05da05a17d1e061e75b93af57f27f58d17
David-LBautista <ing.davlucbau@gmail.com>
2021-09-10 20:49:45 -0500

    Adicion de Angular Material y los componentes a utilizar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//! Angular Material
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule } from "@angular/material/form-field";
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from "@angular/material/input";
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 
@@ -8,6 +23,22 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
   ]
 })
 export class MaterialModule { }
--- src/app/usuarios/usuarios.module.ts
+++ src/app/usuarios/usuarios.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//Material
+import { MaterialModule } from '../material/material.module';
+
+//Routing
 import { UsuariosRoutingModule } from './usuarios-routing.module';
+
+// Components
 import { HomeComponent } from './pages/home/home.component';
 import { EditarComponent } from './pages/editar/editar.component';
 
@@ -13,6 +19,7 @@ import { EditarComponent } from './pages/editar/editar.component';
   ],
   imports: [
     CommonModule,
+    MaterialModule,
     UsuariosRoutingModule
   ]
 })
