ff3a98bdff57e197a050b70a130d669a933155fd
David-LBautista <ing.davlucbau@gmail.com>
2021-09-11 00:12:57 -0500

    editar ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/usuarios/pages/editar/editar.component.ts
+++ src/app/usuarios/pages/editar/editar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from "rxjs/operators";
+import { User } from '../../interfaces/usuario.interface';
 
 @Component({
   selector: 'app-editar',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarComponent implements OnInit {
 
-  constructor() { }
+    usuario:User = {
+      id: 0,
+      avatar: '',
+      first_name: '',
+      last_name: '',
+      email: ''
+    }
+
+  constructor(
+
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+
+    if (!this.router.url.includes('editar') ) {
+      return;
+    }
+
+    this.activatedRoute.params
+    .pipe(
+      //switchMap( ({id}) => this.hService.getHeroe(id))
+    )
+    //.subscribe( heroe => this.heroe = heroe)
   }
 
 }
