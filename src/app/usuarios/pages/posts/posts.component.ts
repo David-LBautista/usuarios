40233d07c5c6e58e2119753bd4539bdefb4d91bb
David-LBautista <ing.davlucbau@gmail.com>
2021-09-12 20:08:00 -0500

    obtener lista de posts por id de usuario

--- src/app/usuarios/pages/posts/posts.component.ts
+++ src/app/usuarios/pages/posts/posts.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
+import { User } from '../../interfaces/usuario.interface';
+import { Post } from '../../interfaces/posts.interface';
+import { UsuarioService } from '../../services/usuario.service';
 
 
 @Component({
@@ -8,11 +11,28 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent implements OnInit, OnChanges {
+
+  @Input() user!: User;
+
+  posts: Post[] = [];
 
   constructor(
+    private uService: UsuarioService
   ) { }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.user.currentValue != changes.user.previousValue) {
+      this.user = changes.user.currentValue[0];
 
+      this.uService.getPosts(this.user.id)
+      .subscribe( posts => {
+        this.posts = posts
+        console.log(this.posts)
+      })
+    }
+  }
+  
   ngOnInit(): void {
   }
 
